e4f0a1a5b3c7df8253cd5d85335f0a3288892ec1
dani.hartanto <mochdanihartanto@gmail.com>
2026-02-22 07:02:09 +0700

    wd

--- js/invit_script.js
+++ js/invit_script.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(res => res.json())
     .then(data => {
         document.getElementById("guestName").innerText = data.name;
-        document.getElementById("guestNameInvitation").innerText = data.name;
+        document.getElementById("guestNameInvitation").innerText = data.description;
         document.getElementById("guestAddress").innerText = data.address;
     });
 
